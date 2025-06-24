5f243cf86a6fb25eeeedfef54a90cb00649d9d0a
Dylan Nguyen <nguyendylan2468@gmail.com>
2025-06-24 19:37:06 -0400

    Added Steam username via steam id + user avatars

--- app.js
+++ app.js
@@ -1,59 +1,80 @@
 // Steam ID Test: 76561198274029909 (Swag Boss Jr)
-const stats_API_URL = "http://localhost:8888/results?steamid="
-const userData_API_URL = "http://localhost:8888/player?steamid="
+const stats_API_URL = "http://localhost:8888/results?steamid=";
+const userData_API_URL = "http://localhost:8888/player?steamid=";
 
 // User Data
-let userData = {}
-let steamUserData = {}
+let userData = {};
+let steamUserData = {};
 
 async function userSearch() {
     // Username input
-    const userNameInput = document.querySelector("#floatingInputGroup1")
-    const value = Number(userNameInput.value)
+    const userNameInput = document.querySelector("#floatingInputGroup1");
+    const value = Number(userNameInput.value);
 
     // let response = await fetch(stats_API_URL + value)
     // let response1 = await fetch(userData_API_URL + value)
 
-    let response = await fetch("http://127.0.0.1:5500/JSON/playerStats.json") // TESTING
+    let response = await fetch("http://127.0.0.1:8080/JSON/playerStats.json"); // TESTING
+    let steamUserResponse = await fetch(
+        "http://127.0.0.1:8080/JSON/steamUserData.json",
+    ); // TESTING
 
-    // let respoonse = await fetch("http://127.0.0.1:5500/JSON/steamUserData.json") // TESTING
-
-    userData = await response.json()
-    // steamUserData = await response1.json() @TODO: Name's instead of steamID 
+    userData = await response.json();
+    steamUserData = await steamUserResponse.json();
 
     // Stores data into local storage
     window.localStorage.setItem("userData", JSON.stringify(userData));
+    window.localStorage.setItem("steamUserData", JSON.stringify(steamUserData));
 
-    // Redirect to dashboard page
-    window.location.href = "dashboard.html"
+    if (userData && steamUserData) {
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Invalid Steam ID");
+    }
 }
 
-
 function displayChart(data) {
-    // user data variables 
-    let username = document.querySelector("#username")
-    let timePlayed = document.querySelector("#time")
-    let kd = document.querySelector("#ratio")
+    // user data
+    let username = document.querySelector("#username");
+    let timePlayed = document.querySelector("#time");
+    let kd = document.querySelector("#ratio");
+    let avatar = document.querySelector("#user-avatar");
 
     // Local storage for fetched results.
-    let userData = JSON.parse(window.localStorage.getItem("userData"))
+    let userData = JSON.parse(window.localStorage.getItem("userData"));
+    let steamUserData = JSON.parse(
+        window.localStorage.getItem("steamUserData"),
+    );
 
-    // Display kill death ratio 
-    let ratio = (userData.playerstats.stats[0].value / userData.playerstats.stats[1].value).toFixed(2)
-    kd.textContent = ratio
+    console.log(steamUserData);
+
+    // Display kill death ratio
+    let ratio = (
+        userData.playerstats.stats[0].value /
+        userData.playerstats.stats[1].value
+    ).toFixed(2);
+
+    kd.textContent = ratio;
+
+    // Display Steam username
+    let steamUsername = steamUserData.response.players[0].personaname;
+    username.textContent = steamUsername;
+
+    // Display Steam avatar
+    let steamAvatar = steamUserData.response.players[0].avatarfull;
+    avatar.setAttribute("src", steamAvatar);
 
     // Canvas
     let myChart = document.querySelectorAll(".myChart");
 
     if (myChart) {
-        let stats = {} 
+        let stats = {};
 
         for (let i = 0; i < userData.playerstats.stats.length; i++) {
-
-            let stat = userData.playerstats.stats[i]
+            let stat = userData.playerstats.stats[i];
 
             if (stat.name.includes("total_kills")) {
-                stats[stat.name] = stat.value
+                stats[stat.name] = stat.value;
             }
         }
 
@@ -61,12 +82,20 @@ function displayChart(data) {
             type: "bar", // Bar chart type
             data: {
                 labels: ["AK47", "DEAGLE", "M4A1", "M4A1-S", "AWP"],
-                datasets: [{
-                    label: "Weapons",
-                    data: [stats.total_kills_ak47, stats.total_kills_deagle, stats.total_kills_m4a1, stats.total_kills_m4a1 ,stats.total_kills_awp] // Fixed data length
-                }],
+                datasets: [
+                    {
+                        label: "Weapons",
+                        data: [
+                            stats.total_kills_ak47,
+                            stats.total_kills_deagle,
+                            stats.total_kills_m4a1,
+                            stats.total_kills_m4a1,
+                            stats.total_kills_awp,
+                        ], // Fixed data length
+                    },
+                ],
             },
-            options: {}
+            options: {},
         });
     } else {
         console.error("Failed to acquire context from the canvas element.");
